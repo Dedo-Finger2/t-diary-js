7d8549c1ade62e6c9355f79bbad1b7f489532e20
antonio mauricio <antonioimportant@gmail.com>
2024-12-23 09:53:19 -0300

    feat: adds dynamic importing components + saving user config in local storage

---
+++ src/components/GitHubConfigForm.jsx
@@ -0,0 +1,95 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function GitHubConfigForm() {
+  const [userConfig, setUserConfig] = useState({
+    apiKey: "",
+    username: "",
+    email: "",
+    repositoryName: "",
+    branchName: "",
+  });
+
+  function handleChangeInputValue(event) {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setUserConfig((previousConfig) => ({
+      ...previousConfig,
+      [name]: value,
+    }));
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    localStorage.setItem("userConfigData", JSON.stringify(userConfig));
+    alert("Config saved!");
+  }
+
+  useEffect(() => {
+    const savedUserConfig = localStorage.getItem("userConfigData");
+    if (savedUserConfig) setUserConfig(JSON.parse(savedUserConfig));
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="api-key">API Key</label>
+          <input
+            onChange={handleChangeInputValue}
+            name="apiKey"
+            value={userConfig.apiKey}
+            type="text"
+            placeholder="API Key..."
+            id="api-key"
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            onChange={handleChangeInputValue}
+            name="username"
+            value={userConfig.username}
+            type="text"
+            placeholder="Unsername..."
+            id="username"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChangeInputValue}
+            name="email"
+            value={userConfig.email}
+            type="email"
+            placeholder="Email..."
+            id="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="repository-name">Repository Name</label>
+          <input
+            onChange={handleChangeInputValue}
+            name="repositoryName"
+            value={userConfig.repositoryName}
+            type="text"
+            placeholder="Repository Name..."
+            id="repository-name"
+          />
+        </div>
+        <div>
+          <label htmlFor="branch-name">Branch Name</label>
+          <input
+            onChange={handleChangeInputValue}
+            name="branchName"
+            value={userConfig.branchName}
+            type="text"
+            placeholder="Branch Name..."
+            id="branch-name"
+          />
+        </div>
+        <button type="submit">Salvar</button>
+      </form>
+    </>
+  );
+}
--- src/pages/Config.page.jsx
+++ src/pages/Config.page.jsx
@@ -1,11 +1,47 @@
+import React, { useState } from "react";
 import { Navbar } from "../components/Navbar";
+import { Suspense } from "react";
 
 export function ConfigPage() {
+  const [PlataformConfig, setPlataformConfig] = useState(null);
+
+  function handleSelectPlataform(event) {
+    const plataform = event.target.value;
+    if (plataform === "Select a plataform...") return;
+    switch (plataform.toLowerCase()) {
+      case "github":
+        setPlataformConfig(
+          React.lazy(() => import("./../components/GitHubConfigForm"))
+        );
+        break;
+      case "gitlab":
+        setPlataformConfig(null);
+        break;
+      default:
+        setPlataformConfig(null);
+    }
+  }
+
   return (
     <>
       <Navbar />
       <h1>Config</h1>
       <hr />
+      <select
+        name="plataform"
+        onChange={handleSelectPlataform}
+        id="config-plataform-select"
+      >
+        <option value="#">Select a plataform...</option>
+        <option value="github">GitHub</option>
+        <option value="gitlab">GitLab</option>
+      </select>
+
+      <section>
+        <Suspense fallback={<div>Loading...</div>}>
+          {PlataformConfig ? <PlataformConfig /> : "Select a plafaform..."}
+        </Suspense>
+      </section>
     </>
   );
 }
