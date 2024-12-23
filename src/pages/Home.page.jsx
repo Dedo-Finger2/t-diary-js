a91b7f7ba14676124a22766c00f6f710c58e715a
antonio mauricio <antonioimportant@gmail.com>
2024-12-23 09:00:08 -0300

    feat: makes sure to not allow users to enter home page without setting up their config first

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import { Navbar } from "../components/Navbar";
+import { useNavigate } from "react-router";
 
 export function HomePage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userConfigData = localStorage.getItem("userConfigData");
+    if (userConfigData === null) navigate("/config");
+  });
+
   return (
     <>
       <Navbar />
