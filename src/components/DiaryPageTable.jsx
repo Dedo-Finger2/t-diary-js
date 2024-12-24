18decc504d82f7c4c36ac2e132c90ee1192ce0c4
antonio mauricio <antonioimportant@gmail.com>
2024-12-24 09:59:16 -0300

    feat: fixes now showing the real word count by adding another atob() around the content

--- src/components/DiaryPageTable.jsx
+++ src/components/DiaryPageTable.jsx
@@ -69,7 +69,7 @@ export function DiaryPageTable() {
   }
 
   function getNumberOfWords(content) {
-    const decryptedContent = atob(content);
+    const decryptedContent = atob(atob(content));
     return decryptedContent.split(" ").length;
   }
 
