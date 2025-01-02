ee129a80a6c0b8c6aeaf83e6acba4f098cd6db34
Antonio Mauricio <antonioimportant@gmail.com>
2025-01-02 16:35:44 -0300

    feat: removes setSearchParams

--- src/components/DiaryPageTable.jsx
+++ src/components/DiaryPageTable.jsx
@@ -13,7 +13,7 @@ export function DiaryPageTable() {
   const [selectedPageToDelete, setSelectedPageToDelete] = useState({});
   const [isDeleting, setIsDeleting] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const [amountOfPages, setAmountOfPages] = useState(0);
 
   const navigate = useNavigate();
