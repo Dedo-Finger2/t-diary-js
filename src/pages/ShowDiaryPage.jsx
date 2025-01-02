b09b79f8efa9c61edd0bf280f96ab44cf2e73dd3
Antonio Mauricio <antonioimportant@gmail.com>
2025-01-02 15:09:36 -0300

    feat: adds going to the next and previous diaries

--- src/pages/ShowDiaryPage.jsx
+++ src/pages/ShowDiaryPage.jsx
@@ -2,47 +2,76 @@ import { useState } from "react";
 import { DiaryContent } from "../components/DiaryContent";
 import { Octokit } from "octokit";
 import { useEffect } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { Navbar } from "../components/Navbar";
 
 export function ShowDiaryPagePage() {
-  const [page, setPage] = useState();
+  const [page, setPage] = useState(null);
+  const [requestFailed, setRequestFailed] = useState(false);
   const { path } = useParams();
 
-  async function fetchData() {
-    const userConfig = JSON.parse(localStorage.getItem("userConfigData"));
-
-    const octokit = new Octokit({
-      auth: userConfig.apiKey,
-    });
-
-    try {
-      const response = await octokit.request(
-        "GET /repos/{owner}/{repo}/contents/{path}?ref={ref}",
-        {
-          owner: userConfig.username,
-          repo: userConfig.repositoryName,
-          path,
-          ref: userConfig.branchName,
-        },
-      );
-
-      setPage(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const diaryDateObject = new Date(path.split(".md")[0]);
+  const redirect = useNavigate();
 
   useEffect(() => {
+    async function fetchData() {
+      const userConfig = JSON.parse(localStorage.getItem("userConfigData"));
+      const octokit = new Octokit({
+        auth: userConfig.apiKey,
+      });
+      try {
+        const response = await octokit.request(
+          "GET /repos/{owner}/{repo}/contents/{path}?ref={ref}",
+          {
+            owner: userConfig.username,
+            repo: userConfig.repositoryName,
+            path,
+            ref: userConfig.branchName,
+          }
+        );
+        setPage(response.data);
+        setRequestFailed(false);
+      } catch (error) {
+        if (error.response.status === 404) {
+          setPage(null);
+          setRequestFailed(true);
+        }
+        console.error(error);
+      }
+    }
+
     fetchData();
-  }, []);
+  }, [path]);
+
+  function handleNextDiary() {
+    const nextFileDate = diaryDateObject;
+    nextFileDate.setDate(nextFileDate.getDate() + 1);
+    const nextDiaryPath = nextFileDate.toISOString().split("T")[0] + ".md";
+    redirect(`/page/${nextDiaryPath}`);
+  }
+
+  function handlePreviousDiary() {
+    const previousFileDate = diaryDateObject;
+    previousFileDate.setDate(previousFileDate.getDate() - 1);
+    const previousDiaryPath =
+      previousFileDate.toISOString().split("T")[0] + ".md";
+    redirect(`/page/${previousDiaryPath}`);
+  }
 
   return (
     <>
       <Navbar />
-      <h1>{page?.name || "loading..."}</h1>
-      <hr />
-      <DiaryContent todayDiary={page} canEdit={false} />
+      <div>
+        <h1>
+          {requestFailed ? "Page Not Found!" : page?.name || "Loading..."}
+        </h1>
+        <hr />
+        <div>
+          <button onClick={handlePreviousDiary}>Previous</button>
+          <button onClick={handleNextDiary}>Next</button>
+        </div>
+        <DiaryContent todayDiary={page} canEdit={false} />
+      </div>
     </>
   );
 }
