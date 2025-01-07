456f3864e153bd81c3b4b64cd1bba90307dd64c9
Antonio Mauricio <antonioimportant@gmail.com>
2025-01-07 18:19:26 -0300

    feat: finishes styling diary card

--- src/components/PageCard.jsx
+++ src/components/PageCard.jsx
@@ -1,3 +1,5 @@
+import { Trash2 } from "lucide-react";
+import { Edit3 } from "lucide-react";
 import PropTypes from "prop-types";
 
 PageCard.propTypes = {
@@ -10,8 +12,14 @@ export function PageCard({ page }) {
       <span className="page-card-title">{page.name}</span>
       <span className="page-card-content">{page.name}</span>
       <div className="page-card-actions">
-        <button>Edit</button>
-        <button>Delete</button>
+        <button className="page-card-action-btn page-card-action-edit-btn">
+          <Edit3 size={18} />
+          Edit
+        </button>
+        <button className="page-card-action-btn page-card-action-delete-btn">
+          <Trash2 size={18} />
+          Delete
+        </button>
       </div>
       <div className="page-card-categories"></div>
     </div>
