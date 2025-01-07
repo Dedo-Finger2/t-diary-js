46720f620f1fb4800d56a0750fc0c367bd87cfd7
Antonio Mauricio <antonioimportant@gmail.com>
2025-01-07 19:14:20 -0300

    feat: adds styles to showDiaryPage page

--- src/pages/ShowDiaryPage.jsx
+++ src/pages/ShowDiaryPage.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import { DiaryContent } from "../components/DiaryContent";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router";
-import { Navbar } from "../components/Navbar";
+import { DiaryPageCards } from "../components/DiaryPagesCards";
+import { Sidebar } from "../components/Sidebar";
 import UserConfig from "../utils/UserConfig.util";
 import { GitHubRepository } from "../model/implementation/GitHubRepository";
+import { StepBack } from "lucide-react";
+import { StepForward } from "lucide-react";
 
 export function ShowDiaryPagePage() {
   const [page, setPage] = useState(null);
@@ -50,21 +53,29 @@ export function ShowDiaryPagePage() {
   }
 
   return (
-    <>
-      <Navbar />
-      <div>
+    <div className="main-container">
+      <Sidebar />
+      <div className="middle-container">
+        <DiaryPageCards />
+      </div>
+      <div className="right-side-container">
         <h1>
           {requestFailed
             ? "Page Not Found!"
             : page?.name.split(".")[0] || "Loading..."}
         </h1>
-        <hr />
-        <div>
-          <button onClick={handlePreviousDiary}>Previous</button>
-          <button onClick={handleNextDiary}>Next</button>
-        </div>
         <DiaryContent todayDiary={page} canEdit={false} />
+        <div className="show-diary-control-btn-container">
+          <button onClick={handlePreviousDiary}>
+            <StepBack />
+            Previous
+          </button>
+          <button onClick={handleNextDiary}>
+            <StepForward />
+            Next
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
